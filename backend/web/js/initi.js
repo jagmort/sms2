e28f0f0645e206f27e8623f9c0db60362c2d47c8
Jag Mort <jagmort@gmail.com>
2020-10-26 15:09:23 +0300

    ip address

--- backend/web/js/initi.js
+++ backend/web/js/initi.js
@@ -1,6 +1,6 @@
 const brunches = {bsh:"Республика Башкортостан", chv:'Филиал в Чувашской Республике', kir:'Кировский филиал', mel:'Филиал в Республике Марий Эл', mrd:'Филиал в Республике Мордовия', nzg:'Нижегородский филиал', orb:'Оренбургский филиал', pnz:'Пензенский филиал', smr:'Самарский филиал', srt:'Саратовский филиал', tts:'Филиал в Республике Татарстан', udm:'Филиал в Удмуртской Республике', uln:'Ульяновский филиал'};
 
-const text = '1. Начало\n2. Кр.3(п.13.4.2)\n3. #time#\n4. \n5. #address#\n6. СПД. #dev# FTTB\n7. ШПД #ports#/#clients#\n8. #argus#\n9. ';
+const text = '1. Начало\n2. Кр.3(п.13.4.2)\n3. #time#\n4. \n5. #address#\n6. СПД. #dev#\n7. ШПД #ports#/#clients#\n8. #argus#\n9. ';
 
 $(document).ready(function() {
     jQuery('#w0-kvdate').on('change', function(){
@@ -34,7 +34,7 @@ function createText() {
     var dev = 0, person = 0, legal = 0;
     $('input[id^="row"]:checked').each(function () {
         brunch = $(this).closest('td').siblings().eq(0).html();
-        ip += $(this).closest('td').siblings().eq(2).html() + ', ';
+        ip = $(this).closest('td').siblings().eq(2).html();
         time = $(this).closest('td').siblings().eq(3).html();
         a = $(this).closest('td').siblings().eq(4).find('a').html();
         if(a) argus += a + ', ';
@@ -42,17 +42,18 @@ function createText() {
         person += Number($(this).closest('td').siblings().eq(6).html());
         legal += Number($(this).closest('td').siblings().eq(7).html());
         a = $(this).closest('td').siblings().eq(9).html();
-        if(a) address += a + '; ';
+        if(a) address += a;
+        address += ' [' + ip + ']; '
     });
     $('#text').val(text);
     $('#text').val($('#text').val().replace(/#time#/, time.slice(0, -3)));
     address = brunches[brunch] + '; ' + address.slice(0, -2);
     $('#text').val($('#text').val().replace(/#address#/, address));
     $('#text').val($('#text').val().replace(/#argus#/, argus.slice(0, -2)));
-    if(dev > 4) p6 = 'Недоступно ' + dev + ' коммутаторов';
-    else p6 = 'Недоступно ' + dev + ' коммутатора';
-    if(dev == 1) p6 = 'Недоступен коммутатор';
-
+    if(dev > 4) p6 = 'Недоступно ' + dev + ' коммутаторов FTTB';
+    else p6 = 'Недоступно ' + dev + ' коммутатора FTTB';
+    if(dev == 1) p6 = 'Недоступен коммутатор FTTB';
+    //p6 += ' [' + ip.slice(0, -2) + ']';
     $('#text').val($('#text').val().replace(/#dev#/, p6));
     ports = dev * 24;
     $('#text').val($('#text').val().replace(/#ports#/, ports));
