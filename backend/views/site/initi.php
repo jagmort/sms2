817fcade9e493222d4e3cd9f744e68e28e057795
Jag Mort <jagmort@gmail.com>
2020-10-27 13:10:09 +0300

    gmspd only

--- backend/views/site/initi.php
+++ backend/views/site/initi.php
@@ -6,11 +6,18 @@ $this->registerJsFile('js/jquery-3.2.1.js', ['position' => yii\web\View::POS_HEA
 $this->registerJsFile('js/initi.js', ['position' => yii\web\View::POS_HEAD]);
 $this->registerCssFile('css/main.css');
 
-if(($identity = Yii::$app->user->identity) != NULL):
+if(($identity = Yii::$app->user->identity) != NULL) {
     require('../../send/param.php');
 
-$today = $datetime->format('Y-m-d');
-$first = $datetime->format('Y-m-d');
+    $today = $datetime->format('Y-m-d');
+    $first = $datetime->format('Y-m-d');
+
+    if ($stmt = $db->prepare("SELECT `group`.id AS gid FROM `group`, `user` WHERE user.group_id = `group`.id AND auth_key = ?")) {
+        $stmt->bind_param("s", $identity->getAuthKey());
+        $stmt->execute();
+        $result = $stmt->get_result();
+        $row = $result->fetch_array(MYSQLI_ASSOC);
+        if($row['gid'] == 1) { // gmspd only
 ?>
 <!-- view -->
 <div id="main">
@@ -18,19 +25,19 @@ $first = $datetime->format('Y-m-d');
 <div id="calendar">
 <form id="ajax_form" method="post" action="">
 <?php
-$datetime = new DateTime(null, new DateTimeZone('Europe/Moscow'));
-echo DatePicker::widget([
-    'name' => 'from_date',
-    'value' => $first,
-    'type' => DatePicker::TYPE_RANGE,
-    'name2' => 'to_date',
-    'value2' => $today,
-    'pluginOptions' => [
-        'autoclose'=>true,
-        'format' => 'yyyy-mm-dd'
-    ]
-]);
-if(($identity = Yii::$app->user->identity) != NULL) echo '<input type="hidden" name="authkey" value="' . $identity->getAuthKey() . '" />'
+            $datetime = new DateTime(null, new DateTimeZone('Europe/Moscow'));
+            echo DatePicker::widget([
+                'name' => 'from_date',
+                'value' => $first,
+                'type' => DatePicker::TYPE_RANGE,
+                'name2' => 'to_date',
+                'value2' => $today,
+                'pluginOptions' => [
+                    'autoclose'=>true,
+                    'format' => 'yyyy-mm-dd'
+                ]
+            ]);
+            if(($identity = Yii::$app->user->identity) != NULL) echo '<input type="hidden" name="authkey" value="' . $identity->getAuthKey() . '" />'
 ?>
 <div id="refresh">
 <select name="brunch" id="brunch">
@@ -58,4 +65,8 @@ if(($identity = Yii::$app->user->identity) != NULL) echo '<input type="hidden" n
 </div>
 </div>
 <!-- /view -->
-<?php endif ?>
+<?php
+        }
+    }
+}
+?>
