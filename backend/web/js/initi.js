eca654b9334257a255329543fe19432b4438e1f1
Jag Mort <jagmort@gmail.com>
2020-10-27 07:58:09 +0300

    dslam

--- backend/web/js/initi.js
+++ backend/web/js/initi.js
@@ -1,6 +1,7 @@
 const brunches = {bsh:"Республика Башкортостан", chv:'Филиал в Чувашской Республике', kir:'Кировский филиал', mel:'Филиал в Республике Марий Эл', mrd:'Филиал в Республике Мордовия', nzg:'Нижегородский филиал', orb:'Оренбургский филиал', pnz:'Пензенский филиал', smr:'Самарский филиал', srt:'Саратовский филиал', tts:'Филиал в Республике Татарстан', udm:'Филиал в Удмуртской Республике', uln:'Ульяновский филиал'};
 
 const text = '1. Начало\n2. Кр.3(п.13.4.2)\n3. #time#\n4. \n5. #address#\n6. СПД. #dev#\n7. ШПД #ports#/#clients#\n8. #argus#\n9. ';
+const dev_type = [['DSLAM'], ['Коммутатор доступа FTTx']];
 
 $(document).ready(function() {
     jQuery('#w0-kvdate').on('change', function(){
@@ -19,28 +20,38 @@ $(document).ready(function() {
     });
 
     sendAjaxForm('result_form', 'ajax_form', '/sms2/send/initi.php');
-});
-$(document).on('change', 'input[id="all"]', function() {
-    $('input[id^="row"]').not(this).prop('checked', this.checked);
-    createText();
-});
 
-$(document).on('change', 'input[id^="row"]', function() {
-    createText();
+    $(document).on('change', 'input[id="all"]', function() {
+        $('input[id^="row"]').not(this).prop('checked', this.checked);
+        createText();
+    });
+
+    $(document).on('change', 'input[id^="row"]', function() {
+        createText();
+    });
 });
 
 function createText() {
     var brunch = '', ip = '', time = '', argus = '', address = '';
-    var dev = 0, person = 0, legal = 0;
+    var dev_dslam = 0, dev_fttb = 0, person = 0, legal = 0, port = 0;
     $('input[id^="row"]:checked').each(function () {
         brunch = $(this).closest('td').siblings().eq(0).html();
         ip = $(this).closest('td').siblings().eq(2).html();
         time = $(this).closest('td').siblings().eq(3).html();
         a = $(this).closest('td').siblings().eq(4).find('a').html();
         if(a) argus += a + ', ';
-        dev += Number($(this).closest('td').siblings().eq(5).html()) + 1;
         person += Number($(this).closest('td').siblings().eq(6).html());
         legal += Number($(this).closest('td').siblings().eq(7).html());
+        type = $(this).closest('td').siblings().eq(8).html();
+        d = Number($(this).closest('td').siblings().eq(5).html()) + 1;
+        if(dev_type[0].includes(type)) {
+            dev_dslam += d;
+            port += Math.round(d / 0.75);
+        }
+        if(dev_type[1].includes(type)) {
+            dev_fttb += d;
+            port += d * 24;
+        }
         a = $(this).closest('td').siblings().eq(9).html();
         if(a) address += a;
         address += ' [' + ip + ']; '
@@ -50,13 +61,16 @@ function createText() {
     address = brunches[brunch] + '; ' + address.slice(0, -2);
     $('#text').val($('#text').val().replace(/#address#/, address));
     $('#text').val($('#text').val().replace(/#argus#/, argus.slice(0, -2)));
-    if(dev > 4) p6 = 'Недоступно ' + dev + ' коммутаторов FTTB';
-    else p6 = 'Недоступно ' + dev + ' коммутатора FTTB';
-    if(dev == 1) p6 = 'Недоступен коммутатор FTTB';
+    p6 = 'Недоступно: ';
+    if(dev_fttb > 0) { 
+        p6 += dev_fttb + ' FTTB; ';
+    }
+    if(dev_dslam > 0) { 
+        p6 += dev_dslam + ' DSLAM; ';
+    }
     //p6 += ' [' + ip.slice(0, -2) + ']';
     $('#text').val($('#text').val().replace(/#dev#/, p6));
-    ports = dev * 24;
-    $('#text').val($('#text').val().replace(/#ports#/, ports));
+    $('#text').val($('#text').val().replace(/#ports#/, port));
     clients = person + legal;
     $('#text').val($('#text').val().replace(/#clients#/, clients));
 }
