78378006e2047588404a900ba075dfe77db4626d
Jag Mort <jagmort@gmail.com>
2021-07-10 08:03:56 +0300

    without closed gp

--- send/szo.php
+++ send/szo.php
@@ -3,8 +3,7 @@ require('param.php');
 
 $authkey = $db->real_escape_string($_POST["authkey"]);
 
-if ($stmt2 = $db->prepare("SELECT actual, region, number, comment, exec_section  FROM `argus` WHERE `actual` > `closed` AND `flag` & 4 > 0 ORDER BY actual")) {
-    //$stmt2->bind_param("s", $identity->getAuthKey());
+if ($stmt2 = $db->prepare("SELECT actual, region, number, comment, exec_section, flag FROM argus a INNER JOIN (SELECT `update` FROM `argus` ORDER BY `update` desc LIMIT 1) b ON a.update = b.update WHERE `flag` & 4 > 0 ORDER BY actual")) {
     $stmt2->execute();
     $result2 = $stmt2->get_result();
     while($row2 = $result2->fetch_array(MYSQLI_ASSOC)) {
