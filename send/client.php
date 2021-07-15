1a6bb4a6a7cfc7da190bd88bec26daa7ca640769
Jag Mort <jagmort@gmail.com>
2021-07-15 09:38:12 +0300

    last update

--- send/client.php
+++ send/client.php
@@ -11,7 +11,7 @@ while($row = $result->fetch_array(MYSQLI_ASSOC)) {
 }
 $stmt->free_result();
 
-if ($stmt2 = $db->prepare("SELECT branch AS `Филиал`, `Номер ГП`, minlevel AS `Уровень`, countcl AS `Клиентов`, actual AS `Начало`, estimated AS `Планируемое`, `Тип населенного пункта`, region AS `Регион`, exec_section AS `Очередь`, comment AS `Комментарий`, put AS `e-mail/telegram` FROM sms a RIGHT JOIN (SELECT branch, argus, number, `Номер ГП`, (SELECT name FROM level d WHERE d.id = MIN(level.id)) AS minlevel, COUNT(client.id) AS countcl, actual, estimated, closed, `Тип населенного пункта`, region, exec_section, comment, c.`update` FROM client, level, argus c INNER JOIN (SELECT `update` FROM `argus` ORDER BY `update` desc LIMIT 1) d ON c.`update` = d.`update` WHERE actual > closed AND number = `Номер ГП` AND level.name = client.`Уровень обслуживания` AND (`Номер ГП` LIKE 'ГП СПД-%' OR `Номер ГП` LIKE 'ПРМОН-%') GROUP BY `Номер ГП`) b ON a.argus = b.argus WHERE minlevel = 'ПЛАТИНОВЫЙ' ORDER BY `Начало` ASC")) {
+if ($stmt2 = $db->prepare("SELECT branch AS `Филиал`, `Номер ГП`, minlevel AS `Уровень`, countcl AS `Клиентов`, actual AS `Начало`, estimated AS `Планируемое`, `Тип населенного пункта`, region AS `Регион`, exec_section AS `Очередь`, comment AS `Комментарий`, put AS `e-mail/telegram`, `last` FROM sms a RIGHT JOIN (SELECT branch, argus, number, `Номер ГП`, (SELECT name FROM level d WHERE d.id = MIN(level.id)) AS minlevel, COUNT(client.id) AS countcl, actual, estimated, closed, `Тип населенного пункта`, region, exec_section, comment, c.`update` AS `last` FROM client, level, argus c INNER JOIN (SELECT `update` FROM `argus` WHERE actual > closed ORDER BY `update` desc LIMIT 1) d ON c.`update` = d.`update` WHERE number = `Номер ГП` AND level.name = client.`Уровень обслуживания` AND (`Номер ГП` LIKE 'ГП СПД-%' OR `Номер ГП` LIKE 'ПРМОН-%') GROUP BY `Номер ГП`) b ON a.argus = b.argus WHERE minlevel = 'ПЛАТИНОВЫЙ' ORDER BY `Начало` ASC")) {
     $stmt2->execute();
     $result2 = $stmt2->get_result();
     while($row2 = $result2->fetch_array(MYSQLI_ASSOC)) {
@@ -24,6 +24,8 @@ if ($stmt2 = $db->prepare("SELECT branch AS `Филиал`, `Номер ГП`, m
             $sms = '';
         $now = new DateTime(null, new DateTimeZone('Europe/Moscow'));
         echo '<tr><td><a href="http://omssis-sms.mts-nn.ru/post/copy.php?argus=' . $row2['Номер ГП']. '" target="_blank">' . $row2['Номер ГП']. '</a></td><td>' . $row2['Уровень'] . '</td><td>' . $row2['Клиентов'] . '</td><td>' . $actual->format('d.m.Y H:i') . '</td><td' . ($now > $estimated ? ' class="far"' : '') . '>' . $estimated->format('d.m.Y H:i') . '</td><td>' . $row2['Тип населенного пункта'] . '</td><td>' . $row2['Регион'] . '</td><td>' . $row2['Очередь'] . '</td><td>' . str_replace(array("\n\n"), "<br />", $row2['Комментарий']) . '</td><td>' . ($sms !== '' ? $sms->format('d.m.Y H:i') : '–') . "</td></tr>\n";
+        $last = $row2['last'];
     }
+    echo '<tr><td colspan="10">Последнее обновление: ' . $last . '</td></tr>';
 }
 ?>
--- send/szo.php
+++ send/szo.php
@@ -11,7 +11,7 @@ while($row = $result->fetch_array(MYSQLI_ASSOC)) {
 }
 $stmt->free_result();
 
-if ($stmt2 = $db->prepare("SELECT actual, closed, estimated, region, number, comment, exec_section, branch, flag FROM argus a INNER JOIN (SELECT `update` FROM `argus` ORDER BY `update` desc LIMIT 1) b ON a.update = b.update WHERE `actual` > `closed` AND `flag` & 4 > 0 ORDER BY actual")) {
+if ($stmt2 = $db->prepare("SELECT actual, closed, estimated, region, number, comment, exec_section, branch, flag, a.`update` AS `last` FROM argus a INNER JOIN (SELECT `update` FROM `argus` WHERE `actual` > `closed` ORDER BY `update` desc LIMIT 1) b ON a.update = b.update WHERE `flag` & 4 > 0 ORDER BY actual")) {
     $stmt2->execute();
     $result2 = $stmt2->get_result();
     while($row2 = $result2->fetch_array(MYSQLI_ASSOC)) {
@@ -20,6 +20,8 @@ if ($stmt2 = $db->prepare("SELECT actual, closed, estimated, region, number, com
         $actual = new DateTime($row2['actual']);
         $now = new DateTime(null, new DateTimeZone('Europe/Moscow'));
         echo '<tr><td' . ($now > $estimated ? ' class="far"' : '') . '>' . $actual->format('d.m.Y H:i') . '</td><td>' . $row2['region'] . '</td><td><a href="http://omssis-sms.mts-nn.ru/post/copy.php?argus=' . $row2['number']. '" target="_blank">' . $row2['number']. '</a></td><td>' . str_replace(array("\n\n"), "<br />", $row2['comment']) . '</td><td>' . $row2['exec_section'] . "</td></tr>\n";
+        $last = $row2['last'];
     }
+    echo '<tr><td colspan="5">Последнее обновление: ' . $last . '</td></tr>';
 }
 ?>
